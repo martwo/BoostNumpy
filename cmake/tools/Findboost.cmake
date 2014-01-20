3d9e28761e24fba47f8816d9486c430eb94812a9
Martin Wolf <martin.wolf@fysik.su.se>
2014-01-20 10:23:28 +0000

    update

---
+++ cmake/tools/Findboost.cmake
@@ -0,0 +1,13 @@
+# CMake find script to search for the Boost library.
+
+set(Boost_USE_STATIC_LIBS OFF)
+set(Boost_USE_MULTITHREADED ON)
+set(Boost_USE_STATIC_RUNTIME OFF)
+
+find_package(Boost "1.38.0" COMPONENTS python system thread)
+
+if(Boost_FOUND)
+    message(STATUS "Boost found.")
+    message(STATUS "+    include path: ${Boost_INCLUDE_DIR}")
+    message(STATUS "+    library path: ${Boost_LIBRARIES}")
+endif(Boost_FOUND)
