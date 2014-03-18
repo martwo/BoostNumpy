f807c65346b7c2d2d7759cb525fc471f274dd867
Martin Wolf <martin.wolf@fysik.su.se>
2014-03-18 12:58:11 +0000

    Implement test for unary and binary functions and member function with automatic mapping and wiring.

--- test/dstream_test.py
+++ test/dstream_test.py
@@ -18,16 +18,142 @@ import numpy as np
 class TestDstream(unittest.TestCase):
 
     def test_unary_functions(self):
-        a = np.arange(0,9, dtype=np.float64)
+        a = np.arange(0,100000, dtype=np.float64)
 
         dstream_test_module.unary_to_void__double(a)
+        dstream_test_module.unary_to_void__allow_threads__double(a, nthreads=3)
+        dstream_test_module.unary_to_void__min_thread_size__double(a, nthreads=3)
 
         r = a*a
+
         o = dstream_test_module.unary_to_T_squared__double(a)
         self.assertTrue((o == r).all())
 
-        o = np.empty((9,), dtype=np.float64)
+        o = dstream_test_module.unary_to_T_squared__allow_threads__double(a, nthreads=3)
+        self.assertTrue((o == r).all())
+
+        o = dstream_test_module.unary_to_T_squared__min_thread_size__double(a, nthreads=3)
+        self.assertTrue((o == r).all())
+
+        # Test the out keyword argument.
+        o = np.empty((100000,), dtype=np.float64)
         dstream_test_module.unary_to_T_squared__double(a, out=o)
+        self.assertTrue((o == r).all())
+
+        o = np.empty((100000,), dtype=np.float64)
+        dstream_test_module.unary_to_T_squared__allow_threads__double(a, nthreads=3, out=o)
+        self.assertTrue((o == r).all())
+
+        o = np.empty((100000,), dtype=np.float64)
+        dstream_test_module.unary_to_T_squared__min_thread_size__double(a, nthreads=3, out=o)
+        self.assertTrue((o == r).all())
+
+    def test_binary_functions(self):
+        a = np.arange(0,100000, dtype=np.float64)
+
+        dstream_test_module.binary_to_void__double(a, a)
+        dstream_test_module.binary_to_void__allow_threads__double(a, a, nthreads=3)
+        dstream_test_module.binary_to_void__min_thread_size__double(a, a, nthreads=3)
+
+        r = a*a
+
+        o = dstream_test_module.binary_to_T_mult__double(a, a)
+        self.assertTrue((o == r).all())
+
+        o = dstream_test_module.binary_to_T_mult__allow_threads__double(a, a, nthreads=3)
+        self.assertTrue((o == r).all())
+
+        o = dstream_test_module.binary_to_T_mult__min_thread_size__double(a, a, nthreads=3)
+        self.assertTrue((o == r).all())
+
+        # Test the out keyword argument.
+        o = np.empty((100000,), dtype=np.float64)
+        dstream_test_module.binary_to_T_mult__double(a, a, out=o)
+        self.assertTrue((o == r).all())
+
+        o = np.empty((100000,), dtype=np.float64)
+        dstream_test_module.binary_to_T_mult__allow_threads__double(a, a, nthreads=3, out=o)
+        self.assertTrue((o == r).all())
+
+        o = np.empty((100000,), dtype=np.float64)
+        dstream_test_module.binary_to_T_mult__min_thread_size__double(a, a, nthreads=3, out=o)
+        self.assertTrue((o == r).all())
+
+    def test_unary_methods(self):
+        testclass = dstream_test_module.TestClass()
+
+        a = np.arange(0,100000, dtype=np.float64)
+
+        testclass.unary_to_void__double(a)
+        testclass.unary_to_void__allow_threads__double(a, nthreads=3)
+        testclass.unary_to_void__min_thread_size__double(a, nthreads=3)
+
+        r = a*a
+
+        o = testclass.unary_to_T_squared__double(a)
+        self.assertTrue((o == r).all())
+
+        o = testclass.unary_to_T_squared__allow_threads__double(a, nthreads=3)
+        self.assertTrue((o == r).all())
+
+        o = testclass.unary_to_T_squared__min_thread_size__double(a, nthreads=3)
+        self.assertTrue((o == r).all())
+
+        # Test the out keyword argument.
+        o = np.empty((100000,), dtype=np.float64)
+        testclass.unary_to_T_squared__double(a, out=o)
+        self.assertTrue((o == r).all())
+
+        o = np.empty((100000,), dtype=np.float64)
+        testclass.unary_to_T_squared__allow_threads__double(a, nthreads=3, out=o)
+        self.assertTrue((o == r).all())
+
+        o = np.empty((100000,), dtype=np.float64)
+        testclass.unary_to_T_squared__min_thread_size__double(a, nthreads=3, out=o)
+        self.assertTrue((o == r).all())
+
+    def test_binary_methods(self):
+        testclass = dstream_test_module.TestClass()
+
+        a = np.arange(0,100000, dtype=np.float64)
+
+        testclass.binary_to_void__double(a, a)
+        testclass.binary_to_void__allow_threads__double(a, a, nthreads=3)
+        testclass.binary_to_void__min_thread_size__double(a, a, nthreads=3)
+
+        r = a*a
+
+        o = testclass.binary_to_T_mult__double(a, a)
+        self.assertTrue((o == r).all())
+
+    def test_unary_static_methods(self):
+        a = np.arange(0,100000, dtype=np.float64)
+
+        dstream_test_module.TestClass.static_unary_to_void__double(a)
+
+        r = a*a
+
+        o = dstream_test_module.TestClass.static_unary_to_T_squared__double(a)
+        self.assertTrue((o == r).all())
+
+        # Test the out keyword argument.
+        o = np.empty((100000,), dtype=np.float64)
+        dstream_test_module.TestClass.static_unary_to_T_squared__double(a, out=o)
+        self.assertTrue((o == r).all())
+
+    def test_binary_static_methods(self):
+        a = np.arange(0,100000, dtype=np.float64)
+
+        dstream_test_module.TestClass.static_binary_to_void__double(a, a)
+
+        r = a*a
+
+        o = dstream_test_module.TestClass.static_binary_to_T_mult__double(a, a)
+        self.assertTrue((o == r).all())
+
+        # Test the out keyword argument.
+        o = np.empty((100000,), dtype=np.float64)
+        dstream_test_module.TestClass.static_binary_to_T_mult__double(a, a, out=o)
         self.assertTrue((o == r).all())
 
 if(__name__ == "__main__"):
--- test/dstream_test_module.cpp
+++ test/dstream_test_module.cpp
@@ -16,9 +16,8 @@
  *        Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
  *        http://www.boost.org/LICENSE_1_0.txt).
  */
+#include <boost/shared_ptr.hpp>
 #include <boost/python.hpp>
-
-#define NDEBUG
 
 #include <boost/numpy.hpp>
 #include <boost/numpy/dstream.hpp>
@@ -43,12 +42,150 @@ unary_to_T_squared(T v)
     return v*v;
 }
 
+template <typename T>
+static
+void
+binary_to_void(T, T)
+{}
+
+template <typename T>
+static
+T
+binary_to_T_mult(T v1, T v2)
+{
+    return v1*v2;
+}
+
+struct TestClass
+{
+    template <typename T>
+    static
+    void
+    static_unary_to_void(T)
+    {}
+
+    template <typename T>
+    static
+    void
+    static_binary_to_void(T, T)
+    {}
+
+    template <typename T>
+    static
+    T
+    static_unary_to_T_squared(T v)
+    {
+        return v*v;
+    }
+
+    template <typename T>
+    static
+    T
+    static_binary_to_T_mult(T v1, T v2)
+    {
+        return v1*v2;
+    }
+
+    template <typename T>
+    void
+    unary_to_void(T)
+    {}
+
+    template <typename T>
+    T
+    unary_to_T_squared(T v)
+    {
+        return v*v;
+    }
+
+    template <typename T>
+    void
+    binary_to_void(T, T)
+    {}
+
+    template <typename T>
+    T
+    binary_to_T_mult(T v1, T v2)
+    {
+        return v1*v2;
+    }
+};
+
 }// namespace test
 
 BOOST_PYTHON_MODULE(dstream_test_module)
 {
     bn::initialize();
 
+    // Unary void-return functions.
     ds::def("unary_to_void__double", &test::unary_to_void<double>, bp::arg("v"));
+    ds::def("unary_to_void__allow_threads__double", &test::unary_to_void<double>, bp::arg("v")
+        , ds::allow_threads());
+    ds::def("unary_to_void__min_thread_size__double", &test::unary_to_void<double>, bp::arg("v")
+        , ds::min_thread_size<32>());
+
+    // Binary void-return functions.
+    ds::def("binary_to_void__double", &test::binary_to_void<double>, (bp::args("v1"),"v2"));
+    ds::def("binary_to_void__allow_threads__double", &test::binary_to_void<double>, (bp::args("v1"),"v2")
+        , ds::allow_threads());
+    ds::def("binary_to_void__min_thread_size__double", &test::binary_to_void<double>, (bp::args("v1"),"v2")
+        , ds::min_thread_size<32>());
+
+    // Unary non-void-return functions.
     ds::def("unary_to_T_squared__double", &test::unary_to_T_squared<double>, bp::arg("v"));
+    ds::def("unary_to_T_squared__allow_threads__double", &test::unary_to_T_squared<double>, bp::arg("v")
+        , ds::allow_threads());
+    ds::def("unary_to_T_squared__min_thread_size__double", &test::unary_to_T_squared<double>, bp::arg("v")
+        , ds::min_thread_size<32>());
+
+    // Binary non-void-return functions.
+    ds::def("binary_to_T_mult__double", &test::binary_to_T_mult<double>, (bp::args("v1"),"v2"));
+    ds::def("binary_to_T_mult__allow_threads__double", &test::binary_to_T_mult<double>, (bp::args("v1"),"v2")
+        , ds::allow_threads());
+    ds::def("binary_to_T_mult__min_thread_size__double", &test::binary_to_T_mult<double>, (bp::args("v1"),"v2")
+        , ds::min_thread_size<32>());
+
+    bp::class_<test::TestClass, boost::shared_ptr<test::TestClass> >("TestClass")
+        // Unary void-return methods.
+        .def(ds::method("unary_to_void__double", &test::TestClass::unary_to_void<double>, bp::arg("v")))
+        .def(ds::method("unary_to_void__allow_threads__double", &test::TestClass::unary_to_void<double>, bp::arg("v")
+            , ds::allow_threads()))
+        .def(ds::method("unary_to_void__min_thread_size__double", &test::TestClass::unary_to_void<double>, bp::arg("v")
+            , ds::min_thread_size<32>()))
+
+        // Binary void-return methods.
+        .def(ds::method("binary_to_void__double", &test::TestClass::binary_to_void<double>, (bp::args("v1"),"v2")))
+        .def(ds::method("binary_to_void__allow_threads__double", &test::TestClass::binary_to_void<double>, (bp::args("v1"),"v2")
+            , ds::allow_threads()))
+        .def(ds::method("binary_to_void__min_thread_size__double", &test::TestClass::binary_to_void<double>, (bp::args("v1"),"v2")
+            , ds::min_thread_size<32>()))
+
+        // Unary non-void-return methods.
+        .def(ds::method("unary_to_T_squared__double", &test::TestClass::unary_to_T_squared<double>, bp::arg("v")))
+        .def(ds::method("unary_to_T_squared__allow_threads__double", &test::TestClass::unary_to_T_squared<double>, bp::arg("v")
+            , ds::allow_threads()))
+        .def(ds::method("unary_to_T_squared__min_thread_size__double", &test::TestClass::unary_to_T_squared<double>, bp::arg("v")
+            , ds::min_thread_size<32>()))
+
+        // Binary non-void-return methods.
+        .def(ds::method("binary_to_T_mult__double", &test::TestClass::binary_to_T_mult<double>, (bp::args("v1"),"v2")))
+        .def(ds::method("binary_to_T_mult__allow_threads__double", &test::TestClass::binary_to_T_mult<double>, (bp::args("v1"),"v2")
+            , ds::allow_threads()))
+        .def(ds::method("binary_to_T_mult__min_thread_size__double", &test::TestClass::binary_to_T_mult<double>, (bp::args("v1"),"v2")
+            , ds::min_thread_size<32>()))
+
+        // Note about static methods:
+        //       Since the implementation of the method call of static methods
+        //       is identical to the non-static methods, we need to test only
+        //       one case, i.e. testing for the correct conversion from
+        //       non-static to static methods.
+
+        // Unary static methods.
+        .def(ds::staticmethod("static_unary_to_void__double", &test::TestClass::static_unary_to_void<double>, bp::arg("v")))
+        .def(ds::staticmethod("static_unary_to_T_squared__double", &test::TestClass::static_unary_to_T_squared<double>, bp::arg("v")))
+
+        // Binary static methods.
+        .def(ds::staticmethod("static_binary_to_void__double", &test::TestClass::static_binary_to_void<double>, (bp::args("v1"),"v2")))
+        .def(ds::staticmethod("static_binary_to_T_mult__double", &test::TestClass::static_binary_to_T_mult<double>, (bp::args("v1"),"v2")))
+    ;
 }
