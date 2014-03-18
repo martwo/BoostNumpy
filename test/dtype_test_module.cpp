afc1be77c8f4680a7eb770e8726ab9ec42f2db18
Martin Wolf <martin.wolf@fysik.su.se>
2014-03-18 00:07:04 +0000

    Polish the code.

--- test/dtype_test_module.cpp
+++ test/dtype_test_module.cpp
@@ -44,32 +44,32 @@ BOOST_PYTHON_MODULE(dtype_test_module)
     bp::def("equivalent", bn::dtype::equivalent);
 
     // integers, by number of bits
-    bp::def("accept_int8", &test::accept<boost::int8_t>);
-    bp::def("accept_uint8", &test::accept<boost::uint8_t>);
-    bp::def("accept_int16", &test::accept<boost::int16_t>);
+    bp::def("accept_int8",   &test::accept<boost::int8_t>);
+    bp::def("accept_uint8",  &test::accept<boost::uint8_t>);
+    bp::def("accept_int16",  &test::accept<boost::int16_t>);
     bp::def("accept_uint16", &test::accept<boost::uint16_t>);
-    bp::def("accept_int32", &test::accept<boost::int32_t>);
+    bp::def("accept_int32",  &test::accept<boost::int32_t>);
     bp::def("accept_uint32", &test::accept<boost::uint32_t>);
-    bp::def("accept_int64", &test::accept<boost::int64_t>);
+    bp::def("accept_int64",  &test::accept<boost::int64_t>);
     bp::def("accept_uint64", &test::accept<boost::uint64_t>);
 
     // integers, by C name according to NumPy
-    bp::def("accept_bool_", &test::accept<bool>);
-    bp::def("accept_byte", &test::accept<signed char>);
-    bp::def("accept_ubyte", &test::accept<unsigned char>);
-    bp::def("accept_short", &test::accept<short>);
+    bp::def("accept_bool_",  &test::accept<bool>);
+    bp::def("accept_byte",   &test::accept<signed char>);
+    bp::def("accept_ubyte",  &test::accept<unsigned char>);
+    bp::def("accept_short",  &test::accept<short>);
     bp::def("accept_ushort", &test::accept<unsigned short>);
-    bp::def("accept_intc", &test::accept<int>);
-    bp::def("accept_uintc", &test::accept<unsigned int>);
+    bp::def("accept_intc",   &test::accept<int>);
+    bp::def("accept_uintc",  &test::accept<unsigned int>);
 
     // floats and complex
-    bp::def("accept_float32", &test::accept<float>);
-    bp::def("accept_float64", &test::accept<double>);
-    bp::def("accept_complex64", &test::accept< std::complex<float> >);
+    bp::def("accept_float32",    &test::accept<float>);
+    bp::def("accept_float64",    &test::accept<double>);
+    bp::def("accept_complex64",  &test::accept< std::complex<float> >);
     bp::def("accept_complex128", &test::accept< std::complex<double> >);
     if(sizeof(long double) > sizeof(double))
     {
-        bp::def("accept_longdouble", &test::accept<long double>);
+        bp::def("accept_longdouble",  &test::accept<long double>);
         bp::def("accept_clongdouble", &test::accept< std::complex<long double> >);
     }
 }
