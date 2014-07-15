e36a6139dd68176e3f429b5d144c2c1aeac87ddc
Martin Wolf <martin.wolf@fysik.su.se>
2014-07-15 18:11:02 +0200

    Set version to the upcoming release version.

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -6,7 +6,7 @@ add_definitions(-fPIC)
 
 link_libraries(stdc++)
 
-set(BOOST_NUMPY_VERSION_STRING "1.0.0" CACHE STRING "The boost_numpy version." FORCE)
+set(BOOST_NUMPY_VERSION_STRING "0.1.1" CACHE STRING "The boost_numpy version." FORCE)
 
 message(STATUS "+    BOOST_NUMPY_VERSION: ${BOOST_NUMPY_VERSION_STRING}")
 message(STATUS "+    CMAKE_INSTALL_PREFIX: ${CMAKE_INSTALL_PREFIX}")
