35a1de2713194868894afff752a1f84ede1141bc
Martin Wolf <martin.wolf@fysik.su.se>
2014-09-02 18:27:41 +0200

    Further development of the return_to_core_shape_data converter.

--- include/boost/numpy/dstream/wiring/return_to_core_shape_data.hpp
+++ include/boost/numpy/dstream/wiring/return_to_core_shape_data.hpp
@@ -25,6 +25,7 @@
 
 #include <vector>
 
+#include <boost/assert.hpp>
 #include <boost/mpl/and.hpp>
 #include <boost/mpl/assert.hpp>
 #include <boost/mpl/if.hpp>
@@ -54,30 +55,185 @@ struct return_to_core_shape_data
 
 namespace detail {
 
+//------------------------------------------------------------------------------
+// The scalar_return_to_core_shape_data template is used to put the function's
+// scalar result data into the one and only output array. This makes only sense
+// for scalar out mappings, which is ensured already by the
+// select_return_to_core_shape_data_converter metafunction.
 template <class OutMapping, class RT>
 struct scalar_return_to_core_shape_data
 {
     typedef scalar_return_to_core_shape_data<OutMapping, RT>
             type;
 
+    template <class WiringModelAPI>
     static
-    void
+    bool
     apply(
         RT result
       , numpy::detail::iter & iter
       , std::vector< std::vector<intptr_t> > const & out_core_shapes
     )
     {
+        BOOST_ASSERT((out_core_shapes.size() == 1 && out_core_shapes[0].size() == 1 && out_core_shapes[0][0] == 1));
 
+        typedef typename WiringModelAPI::template out_arr_value_type<0>::type
+                out_arr_value_t;
+
+        out_arr_value_t & out_arr_value = *reinterpret_cast<out_arr_value_t *>(iter.get_data(0));
+        out_arr_value = out_arr_value_t(result);
+
+        return true;
     }
+};
+
+//------------------------------------------------------------------------------
+// The std_vector_of_scalar_return_to_core_shape_data template is used to put
+// the function's result data from a std::vector<scalar> type into OutArity
+// output arrays, (e.g. ND=1).
+template <class OutMapping, class RT, unsigned OutArity>
+struct std_vector_of_scalar_return_to_core_shape_data;
+
+template <class OutMapping, class RT>
+struct std_vector_of_scalar_return_to_core_shape_data<OutMapping, RT, 1>
+{
+    // This implementation is used to put the 1-dimensional result into the
+    // one and only output array (i.e. OutArity = 1).
+    typedef std_vector_of_scalar_return_to_core_shape_data<OutMapping, RT, 1>
+            type;
+
+    template <class WiringModelAPI>
+    static
+    bool
+    apply(
+        RT result
+      , numpy::detail::iter & iter
+      , std::vector< std::vector<intptr_t> > const & out_core_shapes
+    )
+    {
+        BOOST_ASSERT((out_core_shapes.size() == 1 && out_core_shapes[0].size() == 1));
+
+        typedef typename WiringModelAPI::template out_arr_value_type<0>::type
+                out_arr_value_t;
+
+        intptr_t const out_op_value_stride = iter.get_item_size(0);
+        intptr_t const N = out_core_shapes[0][0];
+        BOOST_ASSERT(N == result.size());
+        for(intptr_t i=0; i<N; ++i)
+        {
+            out_arr_value_t & out_arr_value = *reinterpret_cast<out_arr_value_t *>(iter.get_data(0) + i*out_op_value_stride);
+            out_arr_value = out_arr_value_t(result[i]);
+        }
+        return true;
+    }
+};
+
+// FIXME: Here come all the OutArity specializations for the 1-dimensional
+//        function result.
+
+template <class OutMapping, class RT>
+struct select_std_vector_of_scalar_return_to_core_shape_data
+{
+    // At this point we know that RT is std::vector<scalar_type>, i.e. the
+    // function's result is 1-dimensional. Now we need to distribute the result
+    // values according to the out mapping arity.
+    typedef std_vector_of_scalar_return_to_core_shape_data<OutMapping, RT, OutMapping::arity>
+            apply;
+};
+
+//------------------------------------------------------------------------------
+// The nested_std_vector_of_scalar_return_to_core_shape_data template is used
+// to put the function's result data from a ND-nested std::vector of
+// std::vector's of scalar type into the OutArity output arrays. The NestedRT
+// type is the inner most std::vector<scalar> type of the nested RT type.
+template <class OutMapping, class RT, class NestedRT, unsigned ND, unsigned OutArity>
+struct nested_std_vector_of_scalar_return_to_core_shape_data;
+
+template <class OutMapping, class RT, class NestedRT, unsigned ND>
+struct nested_std_vector_of_scalar_return_to_core_shape_data<OutMapping, RT, NestedRT, ND, 1>
+{
+    // This implementation is used to put the ND-dimensional result into the
+    // one and only output array. Thus the number of core shape dimensions must
+    // match ND.
+    typedef nested_std_vector_of_scalar_return_to_core_shape_data<OutMapping, RT, NestedRT, ND, 1>
+            type;
+
+    template <class WiringModelAPI>
+    static
+    bool
+    apply(
+        RT result
+      , numpy::detail::iter & iter
+      , std::vector< std::vector<intptr_t> > const & out_core_shapes
+    )
+    {
+        return true;
+    }
+};
+
+// FIXME: Here come all the OutArity specializations for the ND-dimensional
+//        function result.
+
+template <class OutMapping, class RT, class NestedRT, unsigned ND>
+struct select_nested_std_vector_of_scalar_return_to_core_shape_data
+{
+    // At this point we know that RT is
+    // std::vector< std::vector< ... scalar ... > >.
+    typedef nested_std_vector_of_scalar_return_to_core_shape_data<OutMapping, RT, NestedRT, ND, OutMapping::arity>
+            apply;
+};
+
+template <class OutMapping, class RT, class NestedRT, unsigned ND>
+struct nested_std_vector_return_to_core_shape_data
+{
+    typedef typename NestedRT::value_type
+            vector_value_type;
+    typedef typename remove_reference<vector_value_type>::type
+            vector_bare_value_type;
+
+    typedef typename boost::mpl::if_<
+              typename is_scalar<vector_bare_value_type>::type
+            , typename select_nested_std_vector_of_scalar_return_to_core_shape_data<OutMapping, RT, NestedRT, ND>::apply
+
+            , typename boost::mpl::if_<
+                typename numpy::mpl::is_std_vector<vector_value_type>::type
+              , typename nested_std_vector_return_to_core_shape_data<OutMapping, RT, vector_value_type, ND+1>::type
+
+              , ::boost::numpy::dstream::wiring::converter::return_to_core_shape_data<OutMapping, RT>
+              >::type
+            >::type
+            type;
 };
 
 template <class OutMapping, class RT>
 struct std_vector_return_to_core_shape_data
 {
-    // FIXME: Add some useful implementation.
-    typedef typename ::boost::numpy::dstream::wiring::converter::return_to_core_shape_data<OutMapping, RT>::type
+    typedef typename RT::value_type
+            vector_value_type;
+    typedef typename remove_reference<vector_value_type>::type
+            vector_bare_value_type;
+
+    typedef typename boost::mpl::if_<
+              typename is_scalar<vector_bare_value_type>::type
+            , typename select_std_vector_of_scalar_return_to_core_shape_data<OutMapping, RT>::apply
+
+            // Check if the std::vector's value type is a std::vector again, and
+            // if so, keep track of the number of dimensions.
+            , typename boost::mpl::if_<
+                typename numpy::mpl::is_std_vector<vector_value_type>::type
+              , typename nested_std_vector_return_to_core_shape_data<OutMapping, RT, vector_value_type, 2>::type
+
+              , ::boost::numpy::dstream::wiring::converter::return_to_core_shape_data<OutMapping, RT>
+              >::type
+            >::type
             type;
+};
+
+template <class OutMapping, class RT>
+struct select_std_vector_return_to_core_shape_data
+{
+    typedef typename std_vector_return_to_core_shape_data<OutMapping, RT>::type
+            apply;
 };
 
 template <class OutMapping, class RT>
@@ -99,7 +255,7 @@ struct select_return_to_core_shape_data_converter
 
             , typename boost::mpl::if_<
                 typename numpy::mpl::is_std_vector<bare_rt>::type
-              , std_vector_return_to_core_shape_data<OutMapping, RT>
+              , typename select_std_vector_return_to_core_shape_data<OutMapping, RT>::apply
 
               , ::boost::numpy::dstream::wiring::converter::return_to_core_shape_data<OutMapping, RT>
               >::type
