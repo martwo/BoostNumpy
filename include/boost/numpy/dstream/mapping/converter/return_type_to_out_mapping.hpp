be2f67fe8929a851a5a74c9fc9a226e136c1d16e
Martin Wolf <martin.wolf@fysik.su.se>
2014-09-05 00:39:53 +0200

    Add support for nested std::vector of scalar or bp::object types mapping to an out mapping of arity one having a multi-dimensional core shape.

--- include/boost/numpy/dstream/mapping/converter/return_type_to_out_mapping.hpp
+++ include/boost/numpy/dstream/mapping/converter/return_type_to_out_mapping.hpp
@@ -17,8 +17,12 @@
  *        Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
  *        http://www.boost.org/LICENSE_1_0.txt).
  */
+#if !BOOST_PP_IS_ITERATING
+
 #ifndef BOOST_NUMPY_DSTREAM_MAPPING_CONVERTER_RETURN_TYPE_TO_OUT_MAPPING_HPP_INCLUDED
 #define BOOST_NUMPY_DSTREAM_MAPPING_CONVERTER_RETURN_TYPE_TO_OUT_MAPPING_HPP_INCLUDED
+
+#include <boost/preprocessor/iterate.hpp>
 
 #include <boost/mpl/assert.hpp>
 #include <boost/mpl/if.hpp>
@@ -37,16 +41,8 @@ namespace dstream {
 namespace mapping {
 namespace converter {
 
-namespace detail {
-
-struct return_type_to_out_mapping_type
-{};
-
-}// namespace detail
-
 template <class T, class Enable=void>
 struct return_type_to_out_mapping
-  : detail::return_type_to_out_mapping_type
 {
     // The return_type_to_out_mapping needs to be specialized.
     // Trigger a compilation error with a meaningful message.
@@ -56,46 +52,59 @@ struct return_type_to_out_mapping
 
 namespace detail {
 
-template <class T, class Enable=void>
+template <class T>
 struct void_to_out_mapping
-  : return_type_to_out_mapping_type
 {
     typedef mapping::detail::out<0>::core_shapes<>
             type;
 };
 
-template <class T, class Enable=void>
-struct scalar_to_out_mapping
-  : return_type_to_out_mapping_type
+template <class T>
+struct scalar_return_type_to_out_mapping
 {
     typedef mapping::detail::out<1>::core_shapes< mapping::detail::core_shape<0>::shape<> >
             type;
 };
 
-template <class T, class Enable=void>
-struct std_vector_of_scalar_to_out_mapping
-  : return_type_to_out_mapping_type
+template <class T>
+struct std_vector_of_scalar_return_type_to_out_mapping
 {
     typedef mapping::detail::out<1>::core_shapes< mapping::detail::core_shape<1>::shape< dim::I > >
             type;
 };
 
+template <class T, unsigned nd>
+struct std_vector_return_type_to_out_mapping;
+
+// Define ND specializations for dimensions J to Z, i.e. up to 18 dimensions.
+#define BOOST_PP_ITERATION_PARAMS_1                                            \
+    (4, (1, 18, <boost/numpy/dstream/mapping/converter/return_type_to_out_mapping.hpp>, 1))
+#include BOOST_PP_ITERATE()
+
 template <class T>
 struct select_return_type_to_out_mapping
 {
+    typedef typename remove_reference<T>::type
+            bare_t;
+
     typedef typename boost::mpl::if_<
-              typename is_same<T, void>::type
+              typename is_same<bare_t, void>::type
             , void_to_out_mapping<T>
 
             , typename boost::mpl::if_<
-                typename is_scalar<typename remove_reference<T>::type>::type
-              , scalar_to_out_mapping<T>
+                typename is_scalar<bare_t>::type
+              , scalar_return_type_to_out_mapping<T>
 
               , typename boost::mpl::if_<
-                  typename numpy::mpl::is_std_vector_of_scalar<T>::type
-                , std_vector_of_scalar_to_out_mapping<T>
+                  typename is_same<bare_t, python::object>::type
+                , scalar_return_type_to_out_mapping<T>
 
-                , typename numpy::dstream::mapping::converter::return_type_to_out_mapping<T>
+                , typename boost::mpl::if_<
+                    typename numpy::mpl::is_std_vector<T>::type
+                  , std_vector_return_type_to_out_mapping<T, 1>
+
+                  , numpy::mpl::unspecified
+                  >::type
                 >::type
               >::type
             >::type
@@ -104,16 +113,19 @@ struct select_return_type_to_out_mapping
 
 template <class T>
 struct return_type_to_out_mapping
-  : select_return_type_to_out_mapping<T>::type
 {
     typedef typename select_return_type_to_out_mapping<T>::type
-            base;
-    typedef typename base::type
+            builtin_converter_selector;
+
+    typedef typename boost::mpl::eval_if<
+              typename is_same<typename builtin_converter_selector::type, numpy::mpl::unspecified>::type
+            , ::boost::numpy::dstream::mapping::converter::return_type_to_out_mapping<T>
+            , builtin_converter_selector
+            >::type
             type;
 };
 
 }// namespace detail
-
 }// namespace converter
 }// namespace mapping
 }// namespace dstream
@@ -121,3 +133,46 @@ struct return_type_to_out_mapping
 }// namespace boost
 
 #endif // !BOOST_NUMPY_DSTREAM_MAPPING_CONVERTER_RETURN_TYPE_TO_OUT_MAPPING_HPP_INCLUDED
+#else
+
+#if BOOST_PP_ITERATION_FLAGS() == 1
+
+#define ND BOOST_PP_ITERATION()
+
+template <class T>
+struct std_vector_return_type_to_out_mapping<T, ND>
+{
+    typedef typename remove_reference<T>::type
+            vector_t;
+    typedef typename vector_t::value_type
+            vector_value_t;
+    typedef typename remove_reference<vector_value_t>::type
+            vector_bare_value_t;
+
+    #define BOOST_NUMPY_DSTREAM_DEF(z, n, data) \
+        BOOST_PP_COMMA_IF(n) dim::I - n
+    typedef typename boost::mpl::if_<
+              typename is_scalar<vector_bare_value_t>::type
+            , mapping::detail::out<1>::core_shapes< mapping::detail::core_shape<ND>::shape< BOOST_PP_REPEAT(ND, BOOST_NUMPY_DSTREAM_DEF, ~) > >
+
+            , typename boost::mpl::if_<
+                typename is_same<vector_bare_value_t, python::object>::type
+              , mapping::detail::out<1>::core_shapes< mapping::detail::core_shape<ND>::shape< BOOST_PP_REPEAT(ND, BOOST_NUMPY_DSTREAM_DEF, ~) > >
+
+              , typename boost::mpl::eval_if<
+                  typename numpy::mpl::is_std_vector<vector_bare_value_t>::type
+                , std_vector_return_type_to_out_mapping<vector_bare_value_t, ND+1>
+
+                , numpy::mpl::unspecified
+                >::type
+              >::type
+            >::type
+            type;
+    #undef BOOST_NUMPY_DSTREAM_DEF
+};
+
+#undef ND
+
+#endif // BOOST_PP_ITERATION_FLAGS() == 1
+
+#endif // BOOST_PP_IS_ITERATING
