d99fb57768a45ddcbe22e3f40b1782470e55ef39
Martin Wolf <martin.wolf@fysik.su.se>
2014-12-07 00:29:42 +0100

    Implemented a specialization for iterating over object arrays, where the iterator returns a boost::python::object instead of a reference.

--- include/boost/numpy/flat_iterator.hpp
+++ include/boost/numpy/flat_iterator.hpp
@@ -32,10 +32,8 @@ namespace detail {
 
 inline
 boost::shared_ptr<iter>
-construct_flat_iter(ndarray & arr)
+construct_flat_iter(ndarray & arr, iter_flags_t iter_flags)
 {
-    iter_flags_t iter_flags = iter::flags::REFS_OK::value
-                            | iter::flags::DONT_NEGATE_STRIDES::value;
     intptr_t itershape[1];
     itershape[0] = -1;
     iter_operand_flags_t arr_op_flags = iter_operand::flags::READONLY::value;
@@ -56,23 +54,6 @@ construct_flat_iter(ndarray & arr)
     return iter_ptr;
 }
 
-template <typename ValueType>
-ValueType &
-dereference(boost::shared_ptr<detail::iter> const & iter_ptr)
-{
-    return *reinterpret_cast<ValueType*>(iter_ptr->get_data(0));
-}
-
-inline
-template <>
-boost::python::object &
-dereference(boost::shared_ptr<detail::iter> const & iter_ptr)
-{
-    uintptr_t * data = reinterpret_cast<uintptr_t*>(iter_ptr->get_data(0));
-    boost::python::object obj(boost::python::detail::borrowed_reference(reinterpret_cast<PyObject*>(*data)));
-    return obj;
-}
-
 }//namespace detail
 
 // The ValueType template parameter must be the C++ corresponding type of the
@@ -80,9 +61,10 @@ dereference(boost::shared_ptr<detail::iter> const & iter_ptr)
 template <typename ValueType>
 class flat_iterator
   : public boost::iterator_facade<
-        flat_iterator<ValueType>
-      , ValueType
-      , boost::forward_traversal_tag
+        flat_iterator<ValueType>     // Derived
+      , ValueType                    // Value
+      , boost::forward_traversal_tag // CategoryOrTraversal
+      , ValueType &                  // Reference
     >
 {
   public:
@@ -96,7 +78,8 @@ class flat_iterator
     {
         // Construct a iterator on the heap which keeps the state of the current
         // iteration.
-        this->iter_ptr_ = detail::construct_flat_iter(arr);
+        detail::iter_flags_t iter_flags = detail::iter::flags::DONT_NEGATE_STRIDES::value;
+        this->iter_ptr_ = detail::construct_flat_iter(arr, iter_flags);
     }
 
     void
@@ -134,7 +117,81 @@ class flat_iterator
     dereference() const
     {
         assert(this->iter_ptr_.get());
-        return detail::dereference<ValueType>(this->iter_ptr_);
+        return *reinterpret_cast<ValueType*>(iter_ptr_->get_data(0));
+    }
+
+  private:
+    friend class boost::iterator_core_access;
+
+    boost::shared_ptr<detail::iter> iter_ptr_;
+};
+
+// Specialization for boost::python::object. In this case the dereferencing
+// returns an object (i.e. bp::object) and not a reference.
+template <>
+class flat_iterator<boost::python::object>
+  : public boost::iterator_facade<
+        flat_iterator<boost::python::object> // Derived
+      , boost::python::object                // Value
+      , boost::forward_traversal_tag         // CategoryOrTraversal
+      , boost::python::object                // Reference
+    >
+{
+  public:
+    // The existence of the default constructor is needed by the STL
+    // requirements.
+    flat_iterator()
+      : iter_ptr_(boost::shared_ptr<detail::iter>())
+    {}
+
+    explicit flat_iterator(ndarray & arr)
+    {
+        // Construct a iterator on the heap which keeps the state of the current
+        // iteration.
+        detail::iter_flags_t iter_flags =
+            detail::iter::flags::DONT_NEGATE_STRIDES::value
+          | detail::iter::flags::REFS_OK::value;
+        this->iter_ptr_ = detail::construct_flat_iter(arr, iter_flags);
+    }
+
+    void
+    increment()
+    {
+        if(! iter_ptr_->next())
+        {
+            // We reached the end of the iteration. So we need to put this
+            // iterator into the END state, wich is (by definition) indicated
+            // that iter_ptr_ is not hold a pointer.
+            // Deallocate the iter object by forgetting about it.
+            iter_ptr_ = boost::shared_ptr<detail::iter>();
+        }
+    }
+
+    bool
+    equal(flat_iterator<boost::python::object> const & other) const
+    {
+        detail::iter * const p1 = iter_ptr_.get();
+        detail::iter * const p2 = other.iter_ptr_.get();
+        if(p1 == NULL && p2 == NULL)
+        {
+            return true;
+        }
+        // Check if one of the two iterators is the END state.
+        if(p1 == NULL || p2 == NULL)
+        {
+            return false;
+        }
+        // If the data pointers point to the same address, we are equal.
+        return (iter_ptr_->get_data(0) == other.iter_ptr_->get_data(0));
+    }
+
+    boost::python::object
+    dereference() const
+    {
+        assert(iter_ptr_.get());
+        uintptr_t * data = reinterpret_cast<uintptr_t*>(iter_ptr_->get_data(0));
+        boost::python::object obj(boost::python::detail::borrowed_reference(reinterpret_cast<PyObject*>(*data)));
+        return obj;
     }
 
   private:
